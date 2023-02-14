b73e13d449446ee98a15f806f31c5085d90476c2
HaugenF <124930192+HaugenF@users.noreply.github.com>
2023-02-14 11:17:22 +0100

    Ändrade koden, slippa skriva lotto0, lotto1 osv

--- script.js
+++ script.js
@@ -8,32 +8,23 @@ let lotto7 = document.querySelector(".lotto-7");
 const container = document.getElementById("circle-container");
 const lottoBtn = document.querySelector(".gameBtn");
 const resetBtn = document.querySelector(".resetBtn");
-
+const lottoNr = [lotto1, lotto2, lotto3, lotto4, lotto5, lotto6, lotto7];
 let lottoRad = [0, 0, 0, 0, 0, 0, 0];
 
 lottoBtn.addEventListener("click", function nyLottoRad() {
   for (let i = 0; i < lottoRad.length; i++) {
     lottoRad[i] = Math.floor(Math.random() * 20 + 1);
+    lottoNr[i].innerHTML = lottoRad[i];
   }
-  lotto1.innerHTML = lottoRad[0];
-  lotto2.innerHTML = lottoRad[1];
-  lotto3.innerHTML = lottoRad[2];
-  lotto4.innerHTML = lottoRad[3];
-  lotto5.innerHTML = lottoRad[4];
-  lotto6.innerHTML = lottoRad[5];
-  lotto7.innerHTML = lottoRad[6];
   container.classList.add("gameOn");
   lottoBtn.innerHTML = "Ny rad";
 });
 
 resetBtn.addEventListener("click", function resetLotto() {
-  lotto1.innerHTML = 0;
-  lotto2.innerHTML = 0;
-  lotto3.innerHTML = 0;
-  lotto4.innerHTML = 0;
-  lotto5.innerHTML = 0;
-  lotto6.innerHTML = 0;
-  lotto7.innerHTML = 0;
+  for (let i = 0; i < lottoRad.length; i++) {
+    lottoNr[i].innerHTML = 0;
+  }
+
   container.classList.remove("gameOn");
   lottoBtn.innerHTML = "Spela!";
 });
